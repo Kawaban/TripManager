fc31205e06e966c683e17c082a63a3cc93fecc6d
AliakseiSamoshyn(272 <aliakseisamoshyn@gmail.com>
2024-03-25 22:10:28 +0100

    implemented statistics frontend

--- app/src/main/java/com/example/tripmanager/statisticsactivity/StatisticImagesActivity.java
+++ app/src/main/java/com/example/tripmanager/statisticsactivity/StatisticImagesActivity.java
@@ -1,20 +1,42 @@
 package com.example.tripmanager.statisticsactivity;
 
+import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tripmanager.R;
+import com.example.tripmanager.statisticsactivity.recycleview.RecyclerAdapter;
 
 import java.util.ArrayList;
 
 public class StatisticImagesActivity extends AppCompatActivity{
 
+    private Button returnButton;
+    private RecyclerView recyclerView;
+
+        @SuppressLint("MissingInflatedId")
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_statistics_images);
+            returnButton = findViewById(R.id.buttonReturn);
+            recyclerView = findViewById(R.id.recyclerView);
+
             ArrayList<Uri> images = getIntent().getParcelableArrayListExtra("images");
+
+            RecyclerAdapter adapter = new RecyclerAdapter(images);
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new GridLayoutManager(this,4));
+
+            returnButton.setOnClickListener(v -> {
+                 finish();
+            });
+
+
         }
 
 }
---
+++ app/src/main/java/com/example/tripmanager/statisticsactivity/recycleview/RecyclerAdapter.java
@@ -0,0 +1,49 @@
+package com.example.tripmanager.statisticsactivity.recycleview;
+
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.tripmanager.R;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    private ArrayList<Uri> images;
+
+    public RecyclerAdapter(ArrayList<Uri> images) {
+        this.images = images;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.custom_single_image, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerAdapter.ViewHolder holder, int position) {
+        holder.imageView.setImageURI(images.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.image);
+        }
+    }
+}
